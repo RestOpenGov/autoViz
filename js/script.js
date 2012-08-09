f31aae577c492f53c290baba18c7ee1d65641cc3
Pablo H. Paladino <paladinop@gmail.com>
2012-08-08 22:31:21 -0300

    Usando elastic con datos reales.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-	var ge,kmlTemplate,kmlResuelto,kmlObject,initialized=false;
+	var ge,kmlTemplate,kmlResuelto,kmlObject,initialized=false,consulta;
 
 	loader(true);
 
@@ -18,9 +18,8 @@
 		log('KML template: cargando...');
 		//Load KML template
 		$.ajax({
-		  //url: "lite.kml",
+		  //url: "docs/kml/lite.kml",
 		  url: "docs/kml/provincias_argentina.kml",
-		  cache: false,
 		  dataType: 'text'
 		}).done(function( kml ) {
 			log('KML template: COMPLETO');
@@ -95,32 +94,128 @@
 	
 	function queryElastic(anio,marca){
 		log('RestOpenGov: consultando...');
-		console.log(anio,marca);
-
+		
 		loader(true);
 
 		if(!anio){
 			var def = getDefaultValues();
 			anio = def.anio;
 			marca = def.marca;
+			
 		}
-		//TODO query
+
+		consulta = {anio:anio,marca:marca};
 		
-		//async callback
-		resultElastic();
-		
+		var opengov = new RestOpenGov({dataSource:'test'});
+		opengov.search({ dataset: 'autos-'+anio, query:'' }, resultElastic);
+
 	}
 	
 	function getRandomNumber(){
 		return Math.floor(Math.random()*100000);
 	}
 
-	function resultElastic(){
+	function resultElastic(resp){
 		log('RestOpenGov: RESPUESTA');
 
 		log('Procesando datos...');
+		
+		var prov,
+			results = [],
+			idProv,
+			cant;
+		$(resp).each(function(i,e){
+			prov = e._source['PROVINCIA'];
+			switch(prov){
+				case 'TOTAL':
+				break;
+				case 'CAPITAL FEDERAL':
+					idProv = 1;
+				break;
+				case 'BUENOS AIRES':
+					idProv = 2;
+				break;
+				case 'CATAMARCA':
+					idProv = 3;
+				break;
+				case 'CORDOBA':
+					idProv = 4;
+				break;
+				case 'CORRIENTES':
+					idProv = 5;
+				break;
+				case 'CHACO':
+					idProv = 6;
+				break;
+				case 'CHUBUT':
+					idProv = 7;
+				break;
+				case 'ENTRE RIOS':
+					idProv = 8;
+				break;
+				case 'FORMOSA':
+					idProv = 9;
+				break;
+				case 'JUJUY':
+					idProv = 10;
+				break;
+				case 'LA PAMPA':
+					idProv = 11;
+				break;
+				case 'LA RIOJA':
+					idProv = 12;
+				break;
+				case 'MENDOZA':
+					idProv = 13;
+				break;
+				case 'MISIONES':
+					idProv = 14;
+				break;
+				case 'NEUQUEN':
+					idProv = 15;
+				break;
+				case 'RIO NEGRO':
+					idProv = 16;
+				break;
+				case 'SALTA':
+					idProv = 17;
+				break;				
+				case 'SAN JUAN':
+					idProv = 18;
+				break;
+				case 'SAN LUIS':
+					idProv = 19;
+				break;
+				case 'SANTA CRUZ':
+					idProv = 20;
+				break;
+				case 'SANTA FE':
+					idProv = 21;
+				break;
+				case 'SANTIAGO DEL ESTERO':
+					idProv = 22;
+				break;
+				case 'TUCUMAN':
+					idProv = 23;
+				break;
+				case 'TIERRA DEL FUEGO':
+					idProv = 24;
+				break;
 
-		//TODO estructura para todos
+			}
+			
+			if(consulta.marca!=''){
+				cant = e._source[consulta.marca];
+			}else{
+				cant = e._source['TOTAL'];
+			}
+			
+			if(idProv)
+				results.push({id : idProv, cant : parseInt(cant)});
+		
+		});
+		
+		/*//TODO estructura para todos
 		var result = 
 			[
 			{id : 1, cant : getRandomNumber()},
@@ -147,19 +242,31 @@
 			{id : 22, cant : getRandomNumber()},
 			{id : 23, cant : getRandomNumber()},
 			{id : 24, cant : getRandomNumber()}
-			];
+			];*/
 
-		resolveTemplate(result);
+		resolveTemplate(results);
 	}
 	
 	function resolveTemplate(data){
 		log('Completando template...');
-		var search;
+		var search,msg;
 		kmlResuelto = kmlTemplate;
+		
+		search = new RegExp('QUERY', 'gi');
+		if(consulta.marca==''){
+			msg = "Cantidad TOTAL en "+consulta.anio;
+		} else {
+			msg = "Cantidad " + consulta.marca + " en "+consulta.anio;
+		}
+		kmlResuelto = kmlResuelto.replace(search, msg);
+		
 		$.each(data, function(i, e) { 
-			search = new RegExp('P_'+e.id, 'gi');
-			kmlResuelto = kmlResuelto.replace(search, e.cant);
-		});		
+			search = new RegExp('{P_'+e.id+'_CANT}', 'gi');
+			kmlResuelto = kmlResuelto.replace(search, e.cant+" autos");
+			search = new RegExp('{P_'+e.id+'}', 'gi');
+			kmlResuelto = kmlResuelto.replace(search, (e.cant*50));
+		});
+		
 		visualize();
 		
 	}
